ccdd1060252633062236b92cef9f74600359ee39
NereRusso <russonere01@gmail.com>
2025-04-08 18:34:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const client = new MercadoPagoConfig({
     accessToken: "APP_USR-3992847979749211-040815-9b0211ddfb88f4d9a321fab43bf614e7-2381296632",
 });
 
-const cors = require('cors');
-
 const app = express();
 const port = 3000;
 
